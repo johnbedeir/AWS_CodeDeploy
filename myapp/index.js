fddc361fc7ce627020e4337ff9ed98c5ece062b3
johnbedeir <john.bedeir@gmail.com>
2024-08-20 00:51:56 +0200

    update app

--- myapp/index.js
+++ myapp/index.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+const http = require('http');
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;  // Set the status code to 200 (OK)
+  res.setHeader('Content-Type', 'text/plain');  // Set the content type to plain text
+  res.end('Hello World\n');  // Send "Hello World" as the response
+});
+
+// The server listens on port 80 (HTTP) or any other available port
+const PORT = process.env.PORT || 80;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
